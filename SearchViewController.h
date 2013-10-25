7d86faacd889dd2eed8022cd18b126260e68834d
brettmelton <brettmelton@gmail.com>
2013-10-24 22:03:25 -0700

    Upload test

--- SearchViewController.h
+++ SearchViewController.h
@@ -6,6 +6,8 @@
 //  Copyright (c) 2013 Brett Melton. All rights reserved.
 //
 
+// This View manages the Search Controller
+
 #import <UIKit/UIKit.h>
 
 @interface SearchViewController : UIViewController
--- SearchViewController.m
+++ SearchViewController.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2013 Brett Melton. All rights reserved.
 //
 
+// This View manages the Search Controller
+
 #import "SearchViewController.h"
 
 @interface SearchViewController ()
